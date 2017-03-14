a92574ae3babe2d195ccec2f7bc6336a0bc9e5aa
Dominic Hofer <dominic.hofer@bausoft.ch>
2017-03-14 17:08:43 +0100

    Adds google test and folders

--- flip_loop.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-// Provides a save and easy to understand flip function.
-// The function is slow and meant for testing and debugging.
-uint64_t flip_loop(const uint64_t P, const uint64_t O, const uint8_t move);
