1c5c2b1850bbfe28d219a8b435fbc99adcc133a7
Dominic Hofer <PanicSheep@users.noreply.github.com>
2017-03-15 17:35:55 +0100

    Update test_flip_loop.cpp

--- test/test_flip_loop.cpp
+++ test/test_flip_loop.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include "flip_loop.h"
+#include "gtest.h"
 
-int main()
+TEST (FlipLoopTest, OneFlip) {
+	const uint64_t P = 0xFF00000000000000ULL;
+	const uint64_t O = 0x00FFFFFFFFFFFF7EULL;
+	const uint8_t move = 0;
+	const uint64_t flip = flip_loop(P, O, move);
+	
+	ASSERT_EQ (P & flip, 0);
+	ASSERT_NE (O & flip, flip);
+	ASSERT_EQ (flip, 0x008141211109050300ULL);
+}
+
+int main(int argc, char **argv)
 {
-	std::cout << flip_loop(0xFF00000000000080ULL, 0x00FFFFFFFFFFFF7EULL, 0) << std::endl;
-	return 0;
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
