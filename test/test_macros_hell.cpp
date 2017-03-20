e133edfc393b10debbbed45c39e65a7704bd861a
Dominic Hofer <dohofer86@gmail.com>
2017-03-20 23:46:51 +0100

    Makes code beautiful

--- test/test_macros_hell.cpp
+++ test/test_macros_hell.cpp
@@ -4,7 +4,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // --------------------------------------------------------------------------------------------
 TEST (MacrosHellTest, BitScanLSB_1) {
-	ASSERT_DEATH(BitScanLSB(0), "(.*?)");
+	ASSERT_DEATH (BitScanLSB(0), "(.*?)");
 	ASSERT_EQ (BitScanLSB(1), 0);
 	ASSERT_EQ (BitScanLSB(2), 1);
 	ASSERT_EQ (BitScanLSB(3), 0);
@@ -55,7 +55,7 @@ TEST (MacrosHellTest, BitScanLSB_6) {
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // --------------------------------------------------------------------------------------------
 TEST (MacrosHellTest, BitScanMSB_1) {
-	ASSERT_DEATH(BitScanMSB(0), "(.*?)");
+	ASSERT_DEATH (BitScanMSB(0), "(.*?)");
 	ASSERT_EQ (BitScanMSB(1), 0);
 	ASSERT_EQ (BitScanMSB(2), 1);
 	ASSERT_EQ (BitScanMSB(3), 1);
