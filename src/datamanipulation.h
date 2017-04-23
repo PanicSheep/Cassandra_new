603ece3b11afb0b0c0d45ca82fc90f091cb0c77a
Dominic Hofer <dohofer86@gmail.com>
2017-04-23 22:30:53 +0200

    Adds "void write_to_file(const std::string & filename, Iterator begin, Iterator end)" and tests

--- src/datamanipulation.h
+++ src/datamanipulation.h
@@ -2,9 +2,10 @@
 #include <cstdint>
 #include <cstdio>
 #include <string>
+#include <type_traits>
 #include <vector>
 
-template <class T>
+template <typename T>
 std::vector<T> read_vector(const std::string & filename, std::size_t size = 0xFFFFFFFFFFFFFFFFULL)
 {
 	FILE* file = fopen(filename.c_str(), "rb");
@@ -26,7 +27,7 @@ std::vector<T> read_vector(const std::string & filename, std::size_t size = 0xFF
 	return vec;
 }
 
-template <class T>
+template <typename T>
 void write_to_file(const std::string & filename, const std::vector<T>& vec)
 {
 	FILE* file = fopen(filename.c_str(), "wb");
@@ -34,6 +35,19 @@ void write_to_file(const std::string & filename, const std::vector<T>& vec)
 		throw "File could not be opened.";
 
 	fwrite(&vec[0], sizeof(T), vec.size(), file);
+
+	fclose(file);
+}
+
+template <typename Iterator>
+void write_to_file(const std::string & filename, Iterator begin, Iterator end)
+{
+	FILE* file = fopen(filename.c_str(), "wb");
+	if (!file)
+		throw "File could not be opened.";
+
+	for (auto it = begin; it != end; it++)
+		fwrite(&*it, sizeof(typename Iterator::value_type), 1, file);
 
 	fclose(file);
 }
--- test/test_datamanipulation.cpp
+++ test/test_datamanipulation.cpp
@@ -3,9 +3,10 @@
 #include <cstdint>
 #include <utility>
 #include <vector>
+#include <set>
 
 template <typename T>
-void TestReadWrite(const std::size_t size)
+void TestReadWriteVec(const std::size_t size)
 {
 	{
 		std::vector<T> vec;
@@ -15,24 +16,24 @@ void TestReadWrite(const std::size_t size)
 	}
 	{
 		std::vector<T> vec = read_vector<T>("tmp.vec~");
-		ASSERT_EQ (vec.size(), static_cast<std::size_t>(size));
+		ASSERT_EQ (vec.size(), size);
 		for (std::size_t i = 0; i < size; i++)
 			ASSERT_EQ (vec[i], static_cast<T>(i));
 	}
 }
 
-TEST (Datamanipulation, ReadWrite___int8) { TestReadWrite<  int8_t>(100); }
-TEST (Datamanipulation, ReadWrite__uint8) { TestReadWrite< uint8_t>(100); }
-TEST (Datamanipulation, ReadWrite__int16) { TestReadWrite< int16_t>(100); }
-TEST (Datamanipulation, ReadWrite_uint16) { TestReadWrite<uint16_t>(100); }
-TEST (Datamanipulation, ReadWrite__int32) { TestReadWrite< int32_t>(100); }
-TEST (Datamanipulation, ReadWrite_uint32) { TestReadWrite<uint32_t>(100); }
-TEST (Datamanipulation, ReadWrite__int64) { TestReadWrite< int64_t>(100); }
-TEST (Datamanipulation, ReadWrite_uint64) { TestReadWrite<uint64_t>(100); }
-TEST (Datamanipulation, ReadWrite_double) { TestReadWrite<  double>(100); }
-TEST (Datamanipulation, ReadWrite__float) { TestReadWrite<   float>(100); }
+TEST (Datamanipulation, ReadWriteVec___int8) { TestReadWriteVec<  int8_t>(100); }
+TEST (Datamanipulation, ReadWriteVec__uint8) { TestReadWriteVec< uint8_t>(100); }
+TEST (Datamanipulation, ReadWriteVec__int16) { TestReadWriteVec< int16_t>(100); }
+TEST (Datamanipulation, ReadWriteVec_uint16) { TestReadWriteVec<uint16_t>(100); }
+TEST (Datamanipulation, ReadWriteVec__int32) { TestReadWriteVec< int32_t>(100); }
+TEST (Datamanipulation, ReadWriteVec_uint32) { TestReadWriteVec<uint32_t>(100); }
+TEST (Datamanipulation, ReadWriteVec__int64) { TestReadWriteVec< int64_t>(100); }
+TEST (Datamanipulation, ReadWriteVec_uint64) { TestReadWriteVec<uint64_t>(100); }
+TEST (Datamanipulation, ReadWriteVec_double) { TestReadWriteVec<  double>(100); }
+TEST (Datamanipulation, ReadWriteVec__float) { TestReadWriteVec<   float>(100); }
 
-TEST (Datamanipulation, ReadWrite_class)
+TEST (Datamanipulation, ReadWriteVec_class)
 {
 	struct Cclass
 	{
@@ -47,6 +48,111 @@ TEST (Datamanipulation, ReadWrite_class)
 		for (std::size_t i = 0; i < size; i++)
 			vec.push_back(Cclass(i,i,i));
 		write_to_file("tmp.vec~", vec);
+	}
+	{
+		std::vector<Cclass> vec = read_vector<Cclass>("tmp.vec~");
+		ASSERT_EQ (vec.size(), static_cast<std::size_t>(size));
+		for (std::size_t i = 0; i < size; i++)
+			ASSERT_EQ (vec[i], Cclass(i,i,i));
+	}
+}
+
+template <typename T>
+void TestReadWriteIteratorVec(const std::size_t size)
+{
+	{
+		std::vector<T> vec;
+		for (std::size_t i = 0; i < size; i++)
+			vec.push_back(static_cast<T>(i));
+		write_to_file("tmp.vec~", vec.begin(), vec.end());
+	}
+	{
+		std::vector<T> vec = read_vector<T>("tmp.vec~");
+		ASSERT_EQ (vec.size(), size);
+		for (std::size_t i = 0; i < size; i++)
+			ASSERT_EQ (vec[i], static_cast<T>(i));
+	}
+}
+
+TEST (Datamanipulation, ReadWriteIteratorVec___int8) { TestReadWriteIteratorVec<  int8_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec__uint8) { TestReadWriteIteratorVec< uint8_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec__int16) { TestReadWriteIteratorVec< int16_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec_uint16) { TestReadWriteIteratorVec<uint16_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec__int32) { TestReadWriteIteratorVec< int32_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec_uint32) { TestReadWriteIteratorVec<uint32_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec__int64) { TestReadWriteIteratorVec< int64_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec_uint64) { TestReadWriteIteratorVec<uint64_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec_double) { TestReadWriteIteratorVec<  double>(100); }
+TEST (Datamanipulation, ReadWriteIteratorVec__float) { TestReadWriteIteratorVec<   float>(100); }
+
+TEST (Datamanipulation, ReadWriteIteratorVec_class)
+{
+	struct Cclass
+	{
+		int a; double b; float c;
+		Cclass(int a, double b, float c) : a(a), b(b), c(c) {}
+		Cclass() : Cclass(0,0,0) {}
+		bool operator==(const Cclass& o) const { return (a == o.a) && (b == o.b) && (c == o.c); }
+	};
+	const std::size_t size = 100;
+	{
+		std::vector<Cclass> vec;
+		for (std::size_t i = 0; i < size; i++)
+			vec.push_back(Cclass(i,i,i));
+		write_to_file("tmp.vec~", vec.begin(), vec.end());
+	}
+	{
+		std::vector<Cclass> vec = read_vector<Cclass>("tmp.vec~");
+		ASSERT_EQ (vec.size(), static_cast<std::size_t>(size));
+		for (std::size_t i = 0; i < size; i++)
+			ASSERT_EQ (vec[i], Cclass(i,i,i));
+	}
+}
+
+template <typename T>
+void TestReadWriteIteratorSet(const std::size_t size)
+{
+	{
+		std::set<T> set;
+		for (std::size_t i = 0; i < size; i++)
+			set.insert(static_cast<T>(i));
+		write_to_file("tmp.vec~", set.begin(), set.end());
+	}
+	{
+		std::vector<T> vec = read_vector<T>("tmp.vec~");
+		ASSERT_EQ (vec.size(), size);
+		for (std::size_t i = 0; i < size; i++)
+			ASSERT_EQ (vec[i], static_cast<T>(i));
+	}
+}
+
+TEST (Datamanipulation, ReadWriteIteratorSet___int8) { TestReadWriteIteratorSet<  int8_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet__uint8) { TestReadWriteIteratorSet< uint8_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet__int16) { TestReadWriteIteratorSet< int16_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet_uint16) { TestReadWriteIteratorSet<uint16_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet__int32) { TestReadWriteIteratorSet< int32_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet_uint32) { TestReadWriteIteratorSet<uint32_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet__int64) { TestReadWriteIteratorSet< int64_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet_uint64) { TestReadWriteIteratorSet<uint64_t>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet_double) { TestReadWriteIteratorSet<  double>(100); }
+TEST (Datamanipulation, ReadWriteIteratorSet__float) { TestReadWriteIteratorSet<   float>(100); }
+
+TEST (Datamanipulation, ReadWriteIteratorSet_class)
+{
+	struct Cclass
+	{
+		int a; double b; float c;
+		Cclass(int a, double b, float c) : a(a), b(b), c(c) {}
+		Cclass() : Cclass(0,0,0) {}
+		bool operator==(const Cclass& o) const { return (a == o.a) && (b == o.b) && (c == o.c); }
+		bool operator< (const Cclass& o) const { return (a < o.a) || ((a == o.a) && (b < o.b))  || ((a == o.a) && (b == o.b) && (c < o.c)); }
+	};
+	const std::size_t size = 100;
+	{
+		std::set<Cclass> set;
+		for (std::size_t i = 0; i < size; i++)
+			set.insert(Cclass(i,i,i));
+		write_to_file("tmp.vec~", set.begin(), set.end());
 	}
 	{
 		std::vector<Cclass> vec = read_vector<Cclass>("tmp.vec~");
