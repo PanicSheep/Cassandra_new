c1662b164eddadd0a23347e3d8c8b29d34788489
Dominic Hofer <dominic.hofer@bausoft.ch>
2017-08-04 15:43:48 +0200

    Adds unsigned qualifiers for numerals.

--- test/test_macros_hell.cpp
+++ test/test_macros_hell.cpp
@@ -5,17 +5,17 @@
 // --------------------------------------------------------------------------------------------
 TEST (MacrosHellTest, BitScanLSB_1) {
 	ASSERT_DEATH (BitScanLSB(0), "(.*?)");
-	ASSERT_EQ (BitScanLSB(1), 0);
-	ASSERT_EQ (BitScanLSB(2), 1);
-	ASSERT_EQ (BitScanLSB(3), 0);
-	ASSERT_EQ (BitScanLSB(0x8000000000000000ULL), 63);
+	ASSERT_EQ (BitScanLSB(1), 0u);
+	ASSERT_EQ (BitScanLSB(2), 1u);
+	ASSERT_EQ (BitScanLSB(3), 0u);
+	ASSERT_EQ (BitScanLSB(0x8000000000000000ULL), 63u);
 }
 
 TEST (MacrosHellTest, BitScanLSB_2) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanLSB(index, 1);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 0);
+	ASSERT_EQ (*index, 0u);
 	delete index;
 }
 
@@ -23,7 +23,7 @@ TEST (MacrosHellTest, BitScanLSB_3) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanLSB(index, 2);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 1);
+	ASSERT_EQ (*index, 1u);
 	delete index;
 }
 
@@ -31,7 +31,7 @@ TEST (MacrosHellTest, BitScanLSB_4) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanLSB(index, 3);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 0);
+	ASSERT_EQ (*index, 0u);
 	delete index;
 }
 
@@ -39,14 +39,14 @@ TEST (MacrosHellTest, BitScanLSB_5) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanLSB(index, 0x8000000000000000ULL);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 63);
+	ASSERT_EQ (*index, 63u);
 	delete index;
 }
 
 TEST (MacrosHellTest, BitScanLSB_6) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanLSB(index, 0);
-	ASSERT_EQ (ret, 0);
+	ASSERT_EQ (ret, 0u);
 	delete index;
 }
 // --------------------------------------------------------------------------------------------
@@ -56,17 +56,17 @@ TEST (MacrosHellTest, BitScanLSB_6) {
 // --------------------------------------------------------------------------------------------
 TEST (MacrosHellTest, BitScanMSB_1) {
 	ASSERT_DEATH (BitScanMSB(0), "(.*?)");
-	ASSERT_EQ (BitScanMSB(1), 0);
-	ASSERT_EQ (BitScanMSB(2), 1);
-	ASSERT_EQ (BitScanMSB(3), 1);
-	ASSERT_EQ (BitScanMSB(0x8000000000000000ULL), 63);
+	ASSERT_EQ (BitScanMSB(1), 0u);
+	ASSERT_EQ (BitScanMSB(2), 1u);
+	ASSERT_EQ (BitScanMSB(3), 1u);
+	ASSERT_EQ (BitScanMSB(0x8000000000000000ULL), 63u);
 }
 
 TEST (MacrosHellTest,BitScanMSB_2) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanMSB(index, 1);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 0);
+	ASSERT_EQ (*index, 0u);
 	delete index;
 }
 
@@ -74,7 +74,7 @@ TEST (MacrosHellTest, BitScanMSB_3) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanMSB(index, 2);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 1);
+	ASSERT_EQ (*index, 1u);
 	delete index;
 }
 
@@ -82,7 +82,7 @@ TEST (MacrosHellTest, BitScanMSB_4) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanMSB(index, 3);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 1);
+	ASSERT_EQ (*index, 1u);
 	delete index;
 }
 
@@ -90,14 +90,14 @@ TEST (MacrosHellTest, BitScanMSB_5) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanMSB(index, 0x8000000000000000ULL);
 	ASSERT_NE (ret, 0);
-	ASSERT_EQ (*index, 63);
+	ASSERT_EQ (*index, 63u);
 	delete index;
 }
 
 TEST (MacrosHellTest, BitScanMSB_6) {
 	unsigned long * index = new unsigned long();
 	unsigned char ret = BitScanMSB(index, 0);
-	ASSERT_EQ (ret, 0);
+	ASSERT_EQ (ret, 0u);
 	delete index;
 }
 // --------------------------------------------------------------------------------------------
@@ -107,18 +107,18 @@ TEST (MacrosHellTest, BitScanMSB_6) {
 // --------------------------------------------------------------------------------------------
 TEST (MacrosHellTest, CountLeadingZerosTest) {
 	ASSERT_DEATH (CountLeadingZeros(0), "(.*?)");
-	ASSERT_EQ (CountLeadingZeros(1), 63);
-	ASSERT_EQ (CountLeadingZeros(2), 62);
-	ASSERT_EQ (CountLeadingZeros(3), 62);
-	ASSERT_EQ (CountLeadingZeros(0x8000000000000000ULL), 0);
+	ASSERT_EQ (CountLeadingZeros(1), 63u);
+	ASSERT_EQ (CountLeadingZeros(2), 62u);
+	ASSERT_EQ (CountLeadingZeros(3), 62u);
+	ASSERT_EQ (CountLeadingZeros(0x8000000000000000ULL), 0u);
 }
 
 TEST (MacrosHellTest, CountTrailingZerosTest) {
 	ASSERT_DEATH (CountTrailingZeros(0), "(.*?)");
-	ASSERT_EQ (CountTrailingZeros(1), 0);
-	ASSERT_EQ (CountTrailingZeros(2), 1);
-	ASSERT_EQ (CountTrailingZeros(3), 0);
-	ASSERT_EQ (CountTrailingZeros(0x8000000000000000ULL), 63);
+	ASSERT_EQ (CountTrailingZeros(1), 0u);
+	ASSERT_EQ (CountTrailingZeros(2), 1u);
+	ASSERT_EQ (CountTrailingZeros(3), 0u);
+	ASSERT_EQ (CountTrailingZeros(0x8000000000000000ULL), 63u);
 }
 // --------------------------------------------------------------------------------------------
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -126,36 +126,36 @@ TEST (MacrosHellTest, CountTrailingZerosTest) {
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // --------------------------------------------------------------------------------------------
 TEST (MacrosHellTest, GetLSBTest) {
-	ASSERT_EQ (GetLSB(0), 0);
-	ASSERT_EQ (GetLSB(1), 1);
-	ASSERT_EQ (GetLSB(2), 2);
-	ASSERT_EQ (GetLSB(3), 1);
+	ASSERT_EQ (GetLSB(0), 0u);
+	ASSERT_EQ (GetLSB(1), 1u);
+	ASSERT_EQ (GetLSB(2), 2u);
+	ASSERT_EQ (GetLSB(3), 1u);
 	ASSERT_EQ (GetLSB(0x8000000000000000ULL), 0x8000000000000000ULL);
 }
 
 TEST (MacrosHellTest, GetMSBTest) {
-	ASSERT_EQ (GetMSB(0), 0);
-	ASSERT_EQ (GetMSB(1), 1);
-	ASSERT_EQ (GetMSB(2), 2);
-	ASSERT_EQ (GetMSB(3), 2);
+	ASSERT_EQ (GetMSB(0), 0u);
+	ASSERT_EQ (GetMSB(1), 1u);
+	ASSERT_EQ (GetMSB(2), 2u);
+	ASSERT_EQ (GetMSB(3), 2u);
 	ASSERT_EQ (GetMSB(0x8000000000000000ULL), 0x8000000000000000ULL);
 }
 
 TEST (MacrosHellTest, RemoveLSBTest) {
 	uint64_t a;
-	a = 0; RemoveLSB(a); ASSERT_EQ (a, 0);
-	a = 1; RemoveLSB(a); ASSERT_EQ (a, 0);
-	a = 2; RemoveLSB(a); ASSERT_EQ (a, 0);
-	a = 3; RemoveLSB(a); ASSERT_EQ (a, 2);
+	a = 0; RemoveLSB(a); ASSERT_EQ (a, 0u);
+	a = 1; RemoveLSB(a); ASSERT_EQ (a, 0u);
+	a = 2; RemoveLSB(a); ASSERT_EQ (a, 0u);
+	a = 3; RemoveLSB(a); ASSERT_EQ (a, 2u);
 	a = 0x8000000001000000ULL; RemoveLSB(a); ASSERT_EQ (a, 0x8000000000000000ULL);
 }
 
 TEST (MacrosHellTest, RemoveMSBTest) {
 	uint64_t a;
-	a = 0; RemoveMSB(a); ASSERT_EQ (a, 0);
-	a = 1; RemoveMSB(a); ASSERT_EQ (a, 0);
-	a = 2; RemoveMSB(a); ASSERT_EQ (a, 0);
-	a = 3; RemoveMSB(a); ASSERT_EQ (a, 1);
+	a = 0; RemoveMSB(a); ASSERT_EQ (a, 0u);
+	a = 1; RemoveMSB(a); ASSERT_EQ (a, 0u);
+	a = 2; RemoveMSB(a); ASSERT_EQ (a, 0u);
+	a = 3; RemoveMSB(a); ASSERT_EQ (a, 1u);
 	a = 0x8000000001000000ULL; RemoveMSB(a); ASSERT_EQ (a, 0x0000000001000000ULL);
 }
 // --------------------------------------------------------------------------------------------
@@ -197,19 +197,19 @@ TEST (MacrosHellTest, MakeBitTest) {
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // --------------------------------------------------------------------------------------------
 TEST (MacrosHellTest, PopCountTest) {
-	ASSERT_EQ (PopCount(0), 0);
-	ASSERT_EQ (PopCount(1), 1);
-	ASSERT_EQ (PopCount(2), 1);
-	ASSERT_EQ (PopCount(3), 2);
-	ASSERT_EQ (PopCount(0xFFFFFFFFFFFFFFFFULL), 64);
+	ASSERT_EQ (PopCount(0), 0u);
+	ASSERT_EQ (PopCount(1), 1u);
+	ASSERT_EQ (PopCount(2), 1u);
+	ASSERT_EQ (PopCount(3), 2u);
+	ASSERT_EQ (PopCount(0xFFFFFFFFFFFFFFFFULL), 64u);
 }
 
 TEST (MacrosHellTest, PopCount_max15Test) {
-	ASSERT_EQ (PopCount_max15(0), 0);
-	ASSERT_EQ (PopCount_max15(1), 1);
-	ASSERT_EQ (PopCount_max15(2), 1);
-	ASSERT_EQ (PopCount_max15(3), 2);
-	ASSERT_EQ (PopCount_max15(0xFF000000000000FEULL), 15);
+	ASSERT_EQ (PopCount_max15(0), 0u);
+	ASSERT_EQ (PopCount_max15(1), 1u);
+	ASSERT_EQ (PopCount_max15(2), 1u);
+	ASSERT_EQ (PopCount_max15(3), 2u);
+	ASSERT_EQ (PopCount_max15(0xFF000000000000FEULL), 15u);
 }
 // --------------------------------------------------------------------------------------------
 ///////////////////////////////////////////////////////////////////////////////////////////////
