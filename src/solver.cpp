77d0acf5cc6e65d633a224492ed4ac2625c67c17
Dominic Hofer <dominic.hofer@bausoft.ch>
2017-08-04 16:14:30 +0200

    Makes use of new functions in game and makes it more user friendly.

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,39 +1,90 @@
 #include "configfile.h"
 #include "game.h"
+#include <thread>
 #include <iostream>
 #include <chrono>
 #include <omp.h>
 
-int main(int argc, char* argv[])
+std::size_t ParseRAM(const std::string& s)
 {
+	std::size_t GB = s.find("GB");
+	std::size_t MB = s.find("MB");
+	std::size_t kB = s.find("kB");
+	std::size_t B = s.find("B");
+	if (GB != std::string::npos) return std::stoll(s) * 1024 * 1024 * 1024;
+	if (MB != std::string::npos) return std::stoll(s) * 1024 * 1024;
+	if (kB != std::string::npos) return std::stoll(s) * 1024;
+	if ( B != std::string::npos) return std::stoll(s);
+	return 0;
+}
+
+void PrintHelp()
+{
+	std::cout << "Solves positions in a given file.\n"
+		<< "-f	      filename\n"
+		<< "-n        number of files to process\n"
+		<< "-t        nubmer of threads to use\n"
+		<< "-d        depth\n"
+		<< "-ram      amount of ram to use\n"
+		<< "-noskip   prevents skipping of solved positions\n"
+		<< "-nosave   prevents saving of results\n"
+		<< "-test     runs in test mode\n"
+		<< "-h        prints this help." << std::endl;
+}
+
+int main(int argc, char* argv[])
+{	
+	std::string filename;									// Name of file to process
+	std::size_t n = 0xFFFFFFFFFFFFFFFFULL;					// Number of positions to process
+	std::size_t t = std::thread::hardware_concurrency();	// Number of threads to use
+	int selectivity = NO_SELECTIVITY;
+	unsigned int depth = 99;
+	std::string RAM = "1GB";								// Amount of RAM to use for hashtable
+	bool bTest = false;										// This run is a test
+	bool bSkipSolved = true;								// Skip positions already solved
+	bool bSave = true;										// Save results
+	int verbosity = 0;										// Verbosity of output
+	
+	// Parse input
+	//------------------------------------
+	for (int i = 0; i < argc; i++)
+	{
+		     if (std::string(argv[i]) == "-f") filename = std::string(argv[++i]);
+		else if (std::string(argv[i]) == "-n") n = atoi(argv[++i]);
+		else if (std::string(argv[i]) == "-t") t = atoi(argv[++i]);
+		else if (std::string(argv[i]) == "-d") depth = atoi(argv[++i]);
+		else if (std::string(argv[i]) == "-RAM") RAM = std::string(argv[++i]);
+		else if (std::string(argv[i]) == "-noskip") bSkipSolved = false;
+		else if (std::string(argv[i]) == "-nosave") bSave = false;
+		else if (std::string(argv[i]) == "-test") { bSkipSolved = false; bSave = false; bTest = true; }
+		else if (std::string(argv[i]) == "-verbose") verbosity = atoi(argv[++i]);
+		else if (std::string(argv[i]) == "-h") { PrintHelp(); return 0; }
+	}
+	//------------------------------------
+	
 	// Initialize
+	//------------------------------------
 	Configfile::Initialize(argv[0]);
 	CountLastFlip::Initialize();
 	Stability::Initialize();
-	Pattern::Initialize(false, true);
-	gTT = CHashTable(32 * 1024 * 1024);
-	gTTPV = CHashTable(1024 * 1024);
-	
-	std::string filename;
-	bool bTest = false;
-	
-	// Parse input
-	for (int i = 0; i < argc; i++)
-	{
-		if (std::string(argv[i]) == "-f") filename = std::string(argv[++i]);
-		else if (std::string(argv[i]) == "-test") bTest = true;
-	}
+	Pattern::Initialize();
+	const std::size_t buckets = ParseRAM(RAM) / sizeof(CHashTable::NodeType);
+	gTT = CHashTable(buckets);
+	gTTPV = CHashTable(buckets / 32u);
+	//------------------------------------
 	
 	// Check input
+	//------------------------------------
 	if (HasValidFilenameExtension(filename) == false) {
 		std::cerr << "ERROR: File has no valid filename extension!" << std::endl;
 		return -1;
 	}
+	//------------------------------------
 	
 	// Use input
 	std::string filename_extension = filename.substr(filename.rfind(".") + 1);
 	if (filename_extension == CPosition::FILENAME_EXTENSION)
-	{
+	{	// CPosition
 		auto vec = LoadVector<CPosition>(filename);
 		
 		std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
@@ -42,7 +93,7 @@ int main(int argc, char* argv[])
 		{
 			uint64_t NC = 0;
 			std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
-			auto score = Endgame_PVS::Eval(pos.P, pos.O, NC);
+			auto score = PVS::Eval(pos.P, pos.O, NC, selectivity, depth);
 			std::chrono::high_resolution_clock::time_point endTime = std::chrono::high_resolution_clock::now();
 			std::cout << SignedInt(score) << " " << ThousandsSeparator(NC) << "\t\t(" << ThousandsSeparator(NC * 1000000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count()) << " N/s)" << std::endl;
 			superNC += NC;
@@ -51,23 +102,30 @@ int main(int argc, char* argv[])
 		std::cout << ThousandsSeparator(superNC) << " in " << time_format(endTime - startTime) << "\t\t(" << ThousandsSeparator(superNC * 1000000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count()) << " N/s)" << std::endl;
 	}
 	else if (filename_extension == CPositionScore::FILENAME_EXTENSION)
-	{
+	{	// CPositionScore
 		auto vec = LoadVector<CPositionScore>(filename);
 		
 		std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
 		std::size_t superNC = 0;
-		#pragma omp parallel for reduction(+:superNC)
-		for (int64_t i = 0; i < vec.size(); i++)
+		int64_t j = 0;
+		for (; j < static_cast<int64_t>(vec.size()); j++)
+			if (!(bSkipSolved && vec[j].IsSolved()))
+				break;
+		#pragma omp parallel for schedule(static,1) reduction(+:superNC)
+		for (int64_t i = j; i < static_cast<int64_t>(std::min(vec.size(), j + n)); i++)
 		{
 			uint64_t NC = 0;
-			auto score = Endgame_PVS::Eval(vec[i].P, vec[i].O, NC);
+			if (!(bSkipSolved && vec[i].IsSolved()))
+				vec[i].score = PVS::Eval(vec[i].P, vec[i].O, NC, selectivity, depth);
 			superNC += NC;
 		}
 		std::chrono::high_resolution_clock::time_point endTime = std::chrono::high_resolution_clock::now();
+		
+		if (bSave) SaveVector(filename, vec);
 		std::cout << ThousandsSeparator(superNC) << " in " << time_format(endTime - startTime) << "\t\t(" << ThousandsSeparator(superNC * 1000000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count()) << " N/s)" << std::endl;
 	}
 	else if (filename_extension == "obf")
-	{
+	{	// CPositionAllScore
 		auto vec = LoadVector<CPositionAllScore>(filename);
 
 		std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
@@ -76,7 +134,7 @@ int main(int argc, char* argv[])
 		{
 			uint64_t NC = 0;
 			std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
-			auto score = Endgame_PVS::Eval(vec[i].P, vec[i].O, NC);
+			auto score = PVS::Eval(vec[i].P, vec[i].O, NC, selectivity, depth);
 			std::chrono::high_resolution_clock::time_point endTime = std::chrono::high_resolution_clock::now();
 			std::cout << SignedInt(score) << " " << ThousandsSeparator(NC) << "\t\t(" << ThousandsSeparator(NC * 1000000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count()) << " N/s)" << std::endl;
 			superNC += NC;
