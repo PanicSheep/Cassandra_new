7fe29a5b1b384a10caf6edc3e1a0c7404a3ea1e1
Dominic Hofer <dohofer86@gmail.com>
2017-08-08 00:40:41 +0200

    Updates bootstrap.bat.

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,5 +1,5 @@
 mkdir pos\rnd
 
-:: Generate random positions (3 Min)
-::FOR /L %%e IN (0,1,49) DO x64\Release\posgen.exe -rnd -o pos/rnd/e%%e_1M.psc -n 1000000 -e %%e
+:: Generate random positions (2 Min)
+FOR /L %%e IN (0,1,49) DO x64\Release\posgen.exe -rnd -o pos/rnd/e%%e_1M.psc -n 1000000 -e %%e
 FOR /L %%e IN (50,1,60) DO x64\Release\posgen.exe -all -o pos/rnd/e%%e_all.psc -e %%e
