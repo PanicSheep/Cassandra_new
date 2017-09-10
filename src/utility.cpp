996be8e24cfbc697e016544f2372adb79e76c2d5
PanicSheep <BishopToG6@gmail.com>
2017-09-11 00:16:53 +0200

    Refactoring of 'time_format(std::chrono::milliseconds duration)'.

--- src/utility.cpp
+++ src/utility.cpp
@@ -3,48 +3,22 @@
 //ddd:hh:mm:ss.ccc
 std::string time_format(const std::chrono::milliseconds duration)
 {
-	std::string time = "            .   "; //ddd:hh:mm:ss.ccc
-	static char digit[10] = { '0','1','2','3','4','5','6','7','8','9' };
-
-	typedef std::chrono::duration<int, std::ratio<24 * 3600> > days;
-	long long c = duration.count() % 1000;
-	long long s = std::chrono::duration_cast<std::chrono::seconds>(duration).count() % 60;
-	int m = std::chrono::duration_cast<std::chrono::minutes>(duration).count() % 60;
-	int h = std::chrono::duration_cast<std::chrono::hours>(duration).count() % 24;
-	int d = std::chrono::duration_cast<days>(duration).count();
-
-	if (d / 100)
-		time[0] = digit[d / 100];
-	if (d / 10)
-		time[1] = digit[(d / 10) % 10];
-	if (d)
-	{
-		time[2] = digit[d % 10];
-		time[3] = ':';
-	}
-	if (d || h / 10)
-		time[4] = digit[h / 10];
-	if (d || h)
-	{
-		time[5] = digit[h % 10];
-		time[6] = ':';
-	}
-	if (d || h || m / 10)
-		time[7] = digit[m / 10];
-	if (d || h || m)
-	{
-		time[8] = digit[m % 10];
-		time[9] = ':';
-	}
-	if (d || h || m || s / 10)
-		time[10] = digit[s / 10];
-
-	time[11] = digit[s % 10];
-	time[13] = digit[c / 100];
-	time[14] = digit[(c / 10) % 10];
-	time[15] = digit[c % 10];
-
-	return std::string(time);
+	typedef std::chrono::duration<int, std::ratio<24 * 3600> > days_t;
+	auto millis = duration.count() % 1000;
+	auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration).count() % 60;
+	auto hours = std::chrono::duration_cast<std::chrono::hours>(duration).count() % 24;
+	auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration).count() % 60;
+	auto days = std::chrono::duration_cast<days_t>(duration).count();
+	
+	std::ostringstream oss;
+	oss << std::setfill(' ') << std::setw(3) << days;
+	if (days) oss << ":" << std::setfill('0');
+	oss << std::setw(2) << hours;
+	if (hours) oss << ":" << std::setfill('0');
+	oss << minutes;
+	if (minutes) oss << ":" << std::setfill('0');
+	oss << seconds << "." << std::setfill('0') << std::setw(3) << millis;
+	return oss.str();
 }
 
 std::string short_time_format(std::chrono::duration<long long, std::pico> duration)
