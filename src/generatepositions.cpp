ac429409db5081d17da7f18e81bf346a0657eb21
PanicSheep <BishopToG6@gmail.com>
2017-10-02 02:11:57 +0200

    Improves the speed of the non deterministic version of 'GenerateRandomPositions'.

--- src/generatepositions.cpp
+++ src/generatepositions.cpp
@@ -3,51 +3,45 @@
 #include <mutex>
 #include <omp.h>
 
-std::unordered_set<CPosition> GenerateRandomPositions(const std::size_t numPos, const uint8_t numEmpties, const bool ETH, const std::size_t seedNum)
+class ThreadSavePosSet
 {
-	std::unordered_set<CPosition> pos_set;
-	std::mutex pos_set_mutex;
-	std::atomic<std::size_t> seed;
-	seed.store(seedNum);
-	
-	#pragma omp parallel
-	{
-		auto rnd = std::bind(std::uniform_int_distribution<unsigned int>(0, 64), std::mt19937_64(seed.fetch_add(1)));
-		
-		#pragma omp for
-		for (int64_t i = 0; i < static_cast<int64_t>(numPos); i++)
-		{
-			CPosition pos(ETH);
-			unsigned int plies = pos.EmptyCount() - numEmpties;
-			while (plies > 0)
-			{
-				uint64_t possibles = pos.PossibleMoves();
-				if (!possibles)
-				{
-					pos.PlayStone(64);
-					possibles = pos.PossibleMoves();
-					if (!possibles)
-					{
-						pos.Reset(ETH);
-						plies = pos.EmptyCount() - numEmpties;
-						continue;
-					}
-				}
-				//for (unsigned int k = rnd() % PopCount(possibles); k > 0; k--)
-				//	RemoveLSB(possibles);
-				//pos.PlayStone(BitScanLSB(possibles));
-				pos.PlayStone(BitScanLSB(PDep(1ULL << (rnd() % PopCount(possibles)), possibles)));
-				plies--;
-			}
-			{
-				std::lock_guard<std::mutex> guard(pos_set_mutex);
-				pos_set.insert(pos);
-			}
-		}
-	}
-	
-	auto rnd = std::bind(std::uniform_int_distribution<unsigned int>(0, 64), std::mt19937_64(seed.fetch_add(1)));
-	while (pos_set.size() < numPos)
+	mutable std::mutex mtx;
+	std::unordered_set<CPosition> set;
+public:
+	bool TryInsert(const CPosition& pos);
+	bool TryInsert(const CPosition& pos, std::size_t maxSize);
+	std::size_t size() const;
+	inline const std::unordered_set<CPosition>& GetSet() const { return set; }
+	inline       std::unordered_set<CPosition>  GetSet()       { return set; }
+};
+
+bool ThreadSavePosSet::TryInsert(const CPosition& pos)
+{
+	std::lock_guard<std::mutex> guard(mtx);
+	const bool InsertionTookPlace = set.insert(pos).second;
+	return InsertionTookPlace;
+}
+
+bool ThreadSavePosSet::TryInsert(const CPosition& pos, std::size_t maxSize)
+{
+	std::lock_guard<std::mutex> guard(mtx);
+	if (set.size() > maxSize)
+		return false;
+	const bool InsertionTookPlace = set.insert(pos).second;
+	return InsertionTookPlace;
+}
+
+std::size_t ThreadSavePosSet::size() const
+{
+	std::lock_guard<std::mutex> guard(mtx);
+	return set.size();
+}
+
+void GenerateRandomPositionsThreadFunction(std::size_t SetSize, const uint8_t numEmpties, const bool ETH, ThreadSavePosSet& set, std::size_t seed)
+{
+	auto rnd = std::bind(std::uniform_int_distribution<unsigned int>(0, 64), std::mt19937_64(seed));
+
+	while (set.size() < SetSize)
 	{
 		CPosition pos(ETH);
 		unsigned int plies = pos.EmptyCount() - numEmpties;
@@ -65,20 +59,76 @@ std::unordered_set<CPosition> GenerateRandomPositions(const std::size_t numPos, 
 					continue;
 				}
 			}
-			//for (unsigned int k = rnd() % PopCount(possibles); k > 0; k--)
-			//	RemoveLSB(possibles);
-			//pos.PlayStone(BitScanLSB(possibles));
 			pos.PlayStone(BitScanLSB(PDep(1ULL << (rnd() % PopCount(possibles)), possibles)));
 			plies--;
 		}
-		pos_set.insert(pos);
+		set.TryInsert(pos, SetSize);
 	}
-	return pos_set;
 }
 
+
+// Non deterministic but fast
 std::unordered_set<CPosition> GenerateRandomPositions(const std::size_t numPos, const uint8_t numEmpties, const bool ETH)
 {
-	return GenerateRandomPositions(numPos, numEmpties, ETH, std::chrono::system_clock::now().time_since_epoch().count());
+	ThreadSavePosSet set;
+	std::size_t seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::vector<std::thread> threads;
+
+	for (std::size_t i = 0; i < std::thread::hardware_concurrency() - 1; i++)
+		threads.push_back(std::thread(GenerateRandomPositionsThreadFunction, numPos, numEmpties, ETH, std::ref(set), seed++));
+	GenerateRandomPositionsThreadFunction(numPos, numEmpties, ETH, set, seed++);
+
+	for (auto& it : threads)
+		it.join();
+
+	return set.GetSet();
+}
+
+// Deterministic
+std::unordered_set<CPosition> GenerateRandomPositions(const std::size_t numPos, const uint8_t numEmpties, const bool ETH, const std::size_t seedNum)
+{
+	std::unordered_set<CPosition> pos_set;
+	std::mutex pos_set_mutex;
+	std::atomic<std::size_t> seed;
+	seed.store(seedNum);
+	
+	while (pos_set.size() < numPos)
+	{
+		#pragma omp parallel
+		{
+			auto rnd = std::bind(std::uniform_int_distribution<unsigned int>(0, 64), std::mt19937_64(seed.fetch_add(1)));
+
+			#pragma omp for
+			for (int64_t i = pos_set.size(); i < static_cast<int64_t>(numPos); i++)
+			{
+				CPosition pos(ETH);
+				unsigned int plies = pos.EmptyCount() - numEmpties;
+				while (plies > 0)
+				{
+					uint64_t possibles = pos.PossibleMoves();
+					if (!possibles)
+					{
+						pos.PlayStone(64);
+						possibles = pos.PossibleMoves();
+						if (!possibles)
+						{
+							pos.Reset(ETH);
+							plies = pos.EmptyCount() - numEmpties;
+							continue;
+						}
+					}
+					pos.PlayStone(BitScanLSB(PDep(1ULL << (rnd() % PopCount(possibles)), possibles)));
+					plies--;
+				}
+				{
+					std::lock_guard<std::mutex> guard(pos_set_mutex);
+					pos_set.insert(pos);
+				}
+			}
+		}
+	}
+	
+	return pos_set;
 }
 
 void GenAll(CPosition pos, std::unordered_set<CPosition>& pos_set, const unsigned int depth)
