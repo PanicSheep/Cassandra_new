d0dc40697b1a169eb915188149a590d7dbb05467
PanicSheep <BishopToG6@gmail.com>
2018-04-10 23:28:32 +0200

    Adds more functionality to Solver/Main.cpp.

--- Solver/Main.cpp
+++ Solver/Main.cpp
@@ -3,6 +3,7 @@
 #include "Utility.h"
 #include "PositionGenerator.h"
 #include "Search.h"
+#include <thread>
 #include <iostream>
 #include <iomanip>
 #include <chrono>
@@ -11,65 +12,95 @@
 void help()
 {
 	std::cout << "Expected input:\n"
-		<< "-i filename\n"
+		<< "-f filename\n"
 		<< "-test\n";
 }
 
 int main(int argc, char* argv[])
 {
-	//CPath Input("..\\pos\\fforum-1-19.obf");
-	CPath Input("..\\pos\\fforum-20-39.obf");
-	//CPath Input("..\\pos\\fforum-40-59.obf");
-	bool test = true;
+	CPath filename;
+	std::size_t threads = std::thread::hardware_concurrency();
+	bool test = false;
+	bool print_each_board = false;
 
 	for (int i = 0; i < argc; i++)
 	{
-		if (std::string(argv[i]) == "-i") Input = CPath(argv[++i]);
+		     if (std::string(argv[i]) == "-f") filename = CPath(argv[++i]);
+		else if (std::string(argv[i]) == "-v") { print_each_board = true; }
 		else if (std::string(argv[i]) == "-test") test = true;
 		else if (std::string(argv[i]) == "-h") { help(); return 0; }
 	}
 
-	SingleLineStreamDecorator SingleLineOutput(std::make_unique<iostreamArchive>(nullptr, std::shared_ptr<std::ostream>(&std::cout, [](void*) {})));
-	const auto boards = CBoardCollection(Input);
-	std::vector<Search*> searches;
+	const auto boards = CBoardCollection(filename);
 
-	std::cout << " # | depth|score|       time (s) |      nodes (N) |    N/s     " << std::endl;
-	std::cout << "---+------+-----+----------------+----------------+------------" << std::endl;
-
-	auto startTime = std::chrono::high_resolution_clock::now();
+	auto env = std::make_shared<Environment>(std::make_shared<CConfigurations>(), std::make_shared<CHashTablePVS>(16'000'000));
+	std::vector<std::unique_ptr<Search>> searches;
 	for (std::size_t i = 0; i < boards.size(); i++)
+		searches.emplace_back(std::make_unique<PVSearch>(env));
+
+	if (print_each_board)
+	{
+		std::cout << " # | depth|score|       time (s) |      nodes (N) |    N/s     " << std::endl;
+		std::cout << "---+------+-----+----------------+----------------+------------" << std::endl;
+	}
+
+	const auto startTime = std::chrono::high_resolution_clock::now();
+	#pragma omp parallel for schedule(static, 1) num_threads(threads)
+	for (int64_t i = 0; i < boards.size(); i++)
 	{
 		const auto& board = boards[i];
-		searches.push_back(new PVSearch());
-		auto search = searches.back();
+		std::unique_ptr<Search>& search = searches[i];
 
 		auto startTime = std::chrono::high_resolution_clock::now();
 		auto score = search->Eval(board->GetPosition());
 		auto endTime = std::chrono::high_resolution_clock::now();
 
-		if (test)
+		if (print_each_board)
+		{
 			if (const auto BoardScore = std::dynamic_pointer_cast<CBoardAllMoveScore>(board))
 			{
-				std::cout
-					<< std::setw(3) << i << "|"
-					<< std::setw(6) << board->GetPosition().EmptyCount() << "|"
-					<< (score == BoardScore->MaxScore() ? " " : "#") << DoubleDigitSignedInt(score) << (score == BoardScore->MaxScore() ? " " : "#") << "|"
-					<< std::setw(16) << time_format(endTime - startTime) << "|"
-					<< std::setw(16) << ThousandsSeparator(search->GetNodeCount()) << "|"
-					<< std::setw(12) << ThousandsSeparator(search->GetNodeCount() * 1'000'000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count())
-					<< std::endl;
+				#pragma omp critical
+				{
+					std::cout
+						<< std::setw(3) << i << "|"
+						<< std::setw(6) << board->GetPosition().EmptyCount() << "|"
+						<< (test && score != BoardScore->MaxScore() ? "#" : " ") << DoubleDigitSignedInt(score) << (test && score != BoardScore->MaxScore() ? "#" : " ") << "|"
+						<< std::setw(16) << time_format(endTime - startTime) << "|"
+						<< std::setw(16) << ThousandsSeparator(search->GetNodeCount()) << "|";
+
+					if (std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count() > 0)
+						std::cout << std::setw(12) << ThousandsSeparator(search->GetNodeCount() * 1'000'000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count());
+					std::cout << std::endl;
+				}
 			}
+			else if (const auto BoardScore = std::dynamic_pointer_cast<CBoardScore>(board))
+			{
+				#pragma omp critical
+				{
+					std::cout
+						<< std::setw(3) << i << "|"
+						<< std::setw(6) << board->GetPosition().EmptyCount() << "|"
+						<< " " << DoubleDigitSignedInt(score) << " |"
+						<< std::setw(16) << time_format(endTime - startTime) << "|"
+						<< std::setw(16) << ThousandsSeparator(search->GetNodeCount()) << "|";
+
+					if (std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count() > 0)
+						std::cout << std::setw(12) << ThousandsSeparator(search->GetNodeCount() * 1'000'000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count());
+						std::cout << std::endl;
+				}
+			}
+		}
 	}
-	auto endTime = std::chrono::high_resolution_clock::now();
+	const auto endTime = std::chrono::high_resolution_clock::now();
 
 	std::size_t NodeCounter = 0;
-	for (auto search : searches)
+	for (auto& search : searches)
 		NodeCounter += search->GetNodeCount();
 
 	std::cout << ThousandsSeparator(NodeCounter) << " nodes in " << time_format(endTime - startTime)
 		<< " (" << ThousandsSeparator(NodeCounter * 1'000'000 / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count()) << " N/s)" << std::endl;
-
-	for (auto search : searches)
-		delete search;
+	
+	if (!test)
+		boards.Save(filename);
 	return 0;
 }
