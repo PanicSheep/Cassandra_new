5ee0ac0427b365407a46ac0dd03cf7b835ecba02
PanicSheep <BishopToG6@gmail.com>
2018-11-21 22:49:44 +0100

    Engine: Reorders function declarations.

--- Engine/src/AlphaBetaFailSoftSearch.h
+++ Engine/src/AlphaBetaFailSoftSearch.h
@@ -18,15 +18,14 @@ namespace Search
 		CResult Eval(const CPosition&, CSpecification) override;
 	private:
 		int Eval  (const CPosition&, int alpha, int beta);
-		int Eval_1(const CPosition&, int alpha);
-		int Eval_2(const CPosition&, int alpha, int beta);
-		int Eval_3(const CPosition&, int alpha, int beta);
-		int Eval_4(const CPosition&, int alpha, int beta);
-
 		int Eval_0(const CPosition&);
+		int Eval_1(const CPosition&, int alpha);
 		int Eval_1(const CPosition&, int alpha,           CMove);
+		int Eval_2(const CPosition&, int alpha, int beta);
 		int Eval_2(const CPosition&, int alpha, int beta, CMove, CMove);
+		int Eval_3(const CPosition&, int alpha, int beta);
 		int Eval_3(const CPosition&, int alpha, int beta, CMove, CMove, CMove);
+		int Eval_4(const CPosition&, int alpha, int beta);
 		int Eval_4(const CPosition&, int alpha, int beta, CMove, CMove, CMove, CMove);
 		int Eval_N(const CPosition&, int alpha, int beta);
 	};
--- Engine/src/NegaMaxSearch.h
+++ Engine/src/NegaMaxSearch.h
@@ -17,15 +17,15 @@ namespace Search
 		
 		CResult Eval(const CPosition&, CSpecification) override;
 	private:
-		int Eval_1(const CPosition&);
-		int Eval_2(const CPosition&);
-		int Eval_3(const CPosition&);
-		int Eval_4(const CPosition&);
 
 		int Eval_0(const CPosition&);
+		int Eval_1(const CPosition&);
 		int Eval_1(const CPosition&, CMove);
+		int Eval_2(const CPosition&);
 		int Eval_2(const CPosition&, CMove, CMove);
+		int Eval_3(const CPosition&);
 		int Eval_3(const CPosition&, CMove, CMove, CMove);
+		int Eval_4(const CPosition&);
 		int Eval_4(const CPosition&, CMove, CMove, CMove, CMove);
 		int Eval_N(const CPosition&);
 	};
